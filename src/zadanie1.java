e273517669f1e9017ea250f2a858092bb9abe319
Blazej <blazej.dyjan@wp.pl>
2018-05-17 23:49:13 +0200

    Dodanie nowych dwóch ostatni linii

--- src/zadanie1.java
+++ src/zadanie1.java
@@ -1,12 +1,14 @@
 class zadanie1 {
 
- public static void main(String[] args) {
+    public static void main(String[] args) {
 
-     System.out.println("Uczę");
-     System.out.println("się");
-     System.out.println("programowania");
+        System.out.println("Uczę");
+        System.out.println("się");
+        System.out.println("programowania");
+        System.out.println("i");
+        System.out.println("commitowania");
 
 
- }
+    }
 
 }
